8032d7890ad77afd4f036db03f2d42da005a9bd1
JSOI <jsoi95071@gmail.com>
2022-04-20 19:59:48 +0900

    Giving ApplicationContext & Test

---
+++ core/core/src/test/java/singleton/StatefulServiceTest.java
@@ -0,0 +1,34 @@
+package singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+        int price1 = statefulService1.order("userA", 10000);
+        int price2 = statefulService2.order("userB", 20000);
+
+        System.out.println("price1 = " + price1);
+        System.out.println("price2 = " + price2);
+
+        Assertions.assertThat(price1).isEqualTo(10000);
+
+    }
+
+    static class TestConfig{
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+}
