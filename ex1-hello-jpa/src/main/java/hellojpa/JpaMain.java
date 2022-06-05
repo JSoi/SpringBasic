34823e53ae76efc5d874150f4544a6ed3956b52c
JSOI <jsoiworks@gmail.com>
2022-06-05 14:54:39 +0900

    JPA Practice - EntityManager, Transactions, Persistence

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,28 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
-        Persistence.createEntityManagerFactory("hello");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         // hello는 /META_INF/persistence.xml의 <persistence-unit name="hello">의 name
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
 
+        try {
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAA");
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
     }
 }
+
