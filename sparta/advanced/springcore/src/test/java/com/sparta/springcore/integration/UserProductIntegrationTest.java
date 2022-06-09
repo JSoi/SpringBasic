592079ddb378222f7f11087f65e46cddd08d1710
JSOI <jsoiworks@gmail.com>
2022-06-09 16:06:29 +0900

    TestCode

---
+++ sparta/advanced/springcore/src/test/java/com/sparta/springcore/integration/UserProductIntegrationTest.java
@@ -0,0 +1,7 @@
+package com.sparta.springcore.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserProductIntegrationTest {
+}
