f49872cc739fac1b18f9160acb85837ec9ae1af6
JSOI <jsoiworks@gmail.com>
2022-06-24 19:39:24 +0900

    thymeleaf 실습

---
+++ mvc2/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -0,0 +1,16 @@
+package hello.thymeleafbasic.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+    @GetMapping("/text-basic")
+    public String goBasic(Model model){
+        model.addAttribute("data", "hello spring!");
+        return "/basic/text-basic";
+    }
+}
