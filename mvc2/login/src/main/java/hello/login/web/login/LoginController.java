e567a89f925d84ec1f6868ea260e4817db22fece
JSoi <jsoiworks@gmail.com>
2022-09-29 23:23:06 +0900

    MVC 2 - Cookie

--- mvc2/login/src/main/java/hello/login/web/login/LoginController.java
+++ mvc2/login/src/main/java/hello/login/web/login/LoginController.java
@@ -1,2 +1,61 @@
-package hello.login.web.login;public class LoginController {
+package hello.login.web.login;
+
+import hello.login.domain.login.LoginService;
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class LoginController {
+    private final LoginService loginService;
+
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute LoginForm loginForm) {
+        return "login/loginForm";
+    }
+
+    @PostMapping("/login")
+    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        if (loginMember == null) { // 특정 필드의 로직이 아님
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다");
+            return "login/loginForm";
+        }
+
+        // 쿠키 넣어주기
+        // 세션 쿠키 (쿠키에 시간 정보를 주지 않았으므로 )
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
 }
